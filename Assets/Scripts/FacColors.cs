75472815db0edcdcf9899c0158cb188ef60aad43
Chieppe <chieppe2142@gmail.com>
2018-10-29 16:40:41 -0200

    Identificacao de times
    Agora o time do jogador possui uma cor propria e os times barbaros usam cores aleatorias

--- Assets/Scripts/FacColors.cs
+++ Assets/Scripts/FacColors.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class FacColors : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public Color[] facColors;
+    public Renderer[] objs;
+    private string Fac;
+
+    private void Start() {
+        Fac = this.GetComponent<HexUnit>().Faccao;
+        Recolor();
+    }
+
+    void Recolor(){ 
+        if(Fac=="Visokea") { 
+            foreach (var o in objs) { 
+                o.materials[0].color=facColors[0];
+                o.materials[1].color=facColors[1];
+                o.materials[2].color=facColors[2];
+            }    
+        }
+        else
+            foreach (var o in objs) { 
+                o.materials[0].color=new Color( Random.value, Random.value, Random.value, 1.0f );
+                o.materials[1].color=new Color( Random.value, Random.value, Random.value, 1.0f );
+                o.materials[2].color=new Color( Random.value, Random.value, Random.value, 1.0f );
+            }
+    }
 }
