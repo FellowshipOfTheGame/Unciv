0b14e48506765d7cf6ba2df9a2bc4b3f36e87ddf
vini <vini_carvalho99@hotmail.com>
2018-11-14 10:00:21 -0200

    bug do very low consertado e menu ingame (inicial) feito

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class PauseManager : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
 
     public GameObject Menupanel;//painel de menu de pausa
 
-    public void QuitGame()
+    public void ResturnTitleScreen()
     {
         SceneManager.LoadScene(0);
     }
@@ -19,19 +18,4 @@ public class PauseManager : MonoBehaviour
         Time.timeScale = 1;
         Menupanel.SetActive(false);
     }
-
-    public void PauseGame()
-    {
-        Time.timeScale = 0;
-        Menupanel.SetActive(true);
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            PauseGame();
-        }
-    }
-
 }
