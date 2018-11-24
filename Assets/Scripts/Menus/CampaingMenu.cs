49d2518e410433d1d0e261bf1f1bda841f82e4fa
vini <vini_carvalho99@hotmail.com>
2018-11-24 15:50:57 -0200

    menu atulizado para  a campanha

--- Assets/Scripts/Menus/CampaingMenu.cs
+++ Assets/Scripts/Menus/CampaingMenu.cs
@@ -3,16 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour {
+public class CampaingMenu : MonoBehaviour
+{
 
     public void NewGame()
     {
         SceneManager.LoadScene(1);
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("quit");
-        Application.Quit();
     }
 }
