b3f51b647805cb91c615246df18d837c22cfb97a
Luís Felipe Ribeiro Chaves <pi.luis.f3@usp.br>
2018-12-27 21:12:55 -0200

    Buttons for each facction are added automatically

--- Assets/Scripts/Menus/CampaignMenu.cs
+++ Assets/Scripts/Menus/CampaignMenu.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CampaignMenu : MonoBehaviour {
 
     public GameObject faccionButton;
     private RectTransform position;
+    private List<Faccao> faccoes;
 
     private void Start(){
         //idea: this start function should instantiate all facctions buttons based on Faccao.faccoes
+        faccoes = CampaignControl.faccoes;
+        for (int i = 0; i < faccoes.Count; i++){
+            GameObject aux = Instantiate(faccionButton);
+            aux.transform.SetParent(this.transform, false);
+            aux.transform.SetSiblingIndex(i);
+            aux.GetComponentInChildren<Text>().text = faccoes[i].factionName;
+        }
     }
 
     public void NewGameVisokea () {
