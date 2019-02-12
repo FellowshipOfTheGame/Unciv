040542d0214a88b43029de59969d18f2802fc63a
Luís Felipe Ribeiro Chaves <pi.luis.f3@usp.br>
2019-02-12 14:06:14 -0200

    Deleção de linhas de códigos e scripts não utilizados

--- Assets/Scripts/Menus/MenuManager.cs
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -1,12 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
-    public void NewGame()
-    {
-        SceneManager.LoadScene(1);
-    }
     public void QuitGame()
     {
         Debug.Log("quit");
